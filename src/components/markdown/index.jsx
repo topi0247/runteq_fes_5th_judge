f0cfec46a7c6b2ec2d360488e545b03aa4911034
topi0247 <karubona.ra247@gmail.com>
2024-08-26 20:32:01 +0900

    feat:Markdownをリスト対応

--- src/components/markdown/index.jsx
+++ src/components/markdown/index.jsx
@@ -6,6 +6,9 @@ export default function MarkdownComponent({ content }) {
     <ReactMarkdown
       remarkPlugins={[remarkBreaks]}
       components={{
+        ul: ({ children }) => <ul className="list-disc ml-4">{children}</ul>,
+        ol: ({ children }) => <ol className="list-decimal ml-4">{children}</ol>,
+        li: ({ children }) => <li>{children}</li>,
         p: ({ children }) => <p>{children}</p>,
         a: ({ children, href }) => <a className="text-blue-500 underline hover:opacity-50 transition-all" href={href} target="_blank" rel="noopener noreferrer">{children}</a>,
       }}>
